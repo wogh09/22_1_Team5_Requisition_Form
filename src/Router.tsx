b8898e3497bc8f838b0c36a9897056e87ccfa1f4
Jaeho <benteke@naver.com>
2022-02-08 00:43:40 +0900

    Add: 카드 컴포넌트 메인페이지로 이동

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './main';
-import Cardbox from 'cardbox/Cardbox';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/card" element={<Cardbox />} />
       </Routes>
     </BrowserRouter>
   );
